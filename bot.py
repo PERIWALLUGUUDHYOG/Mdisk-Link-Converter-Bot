4f76838fc6e71ff30205e168870de55b2c6e35de
Anurag Maheshwari <41854065+8769ANURAG@users.noreply.github.com>
2022-09-25 15:51:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,12 +29,12 @@ bot = Client('Doodstream bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi, {message.chat.first_name} !!**\n\n"
-        "**I am your Personal MDisk Link Converter Bot 🤗, Made by @AboutGuru1💞 Send me a MDisk Post to see the Magic 😅**")
+        "**I am your Personal MDisk Link Converter Bot 🤗, Made by @Am_Robots💞 Send me a MDisk Post to see the Magic 😅**")
     
 @bot.on_message(filters.text & filters.private)
 async def Doodstream_uploader(bot, message):
     new_string = str(message.text)
-    conv = await message.reply("Wait Bnaraha Hu Tab Tak @AboutGuru1 Check Karo✋")
+    conv = await message.reply("Wait Bnaraha Hu Tab Tak @Am_Robots Check Karo✋")
     dele = conv["message_id"]
     try:
         Doodstream_link = await multi_Doodstream_up(new_string)
